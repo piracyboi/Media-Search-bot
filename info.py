bded38eac5a9f9a18215f414a9cf94ce174deff7
piracyboi <76645208+piracyboi@users.noreply.github.com>
2021-07-09 04:41:39 +0530

    Update info.py

--- info.py
+++ info.py
@@ -1,32 +1,24 @@
-import re
-from os import environ
-
-id_pattern = re.compile(r'^.\d+$')
-
-# Bot information
-SESSION = environ.get('SESSION', 'Media_search')
-USER_SESSION = environ.get('USER_SESSION', 'User_Bot')
-API_ID = int(environ['API_ID'])
-API_HASH = environ['API_HASH']
-BOT_TOKEN = environ['BOT_TOKEN']
-USERBOT_STRING_SESSION = environ.get('USERBOT_STRING_SESSION')
+SESSION = 'Media_search'
+USER_SESSION = 'User_Bot'
+API_ID = 2794827
+API_HASH = '451acd9d2a71dd82c3221ec33019c759'
+BOT_TOKEN = '1798773929:AAF212yNML7GjMtPGNUWHzVFUmPkfMWve3I'
+USERBOT_STRING_SESSION = ''
 
 # Bot settings
-CACHE_TIME = int(environ.get('CACHE_TIME', 300))
-USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', False))
+CACHE_TIME = 300
+USE_CAPTION_FILTER = False
 
 # Admins, Channels & Users
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ['ADMINS'].split()]
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ['CHANNELS'].split()]
-auth_users = [int(user) if id_pattern.search(user) else user for user in environ.get('AUTH_USERS', '').split()]
-AUTH_USERS = (auth_users + ADMINS) if auth_users else []
-auth_channel = environ.get('AUTH_CHANNEL')
-AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else auth_channel
+ADMINS = ['950362602']
+CHANNELS = [-1001322302012, 950362602]
+AUTH_USERS = []
+AUTH_CHANNEL = [-1001251060773]
 
 # MongoDB information
-DATABASE_URI = environ['DATABASE_URI']
-DATABASE_NAME = environ['DATABASE_NAME']
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
+DATABASE_URI = "mongodb+srv://Uploader4:23132312Piracyftw@cluster0.krshf.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+DATABASE_NAME = 'filmreqbot'
+COLLECTION_NAME = 'Telegram_files'
 
 # Messages
 default_start_msg = """
@@ -41,5 +33,5 @@ Search Here. Contact @Uploader4
 """
 
 START_MSG = environ.get('START_MSG', default_start_msg)
-SHARE_BUTTON_TEXT = 'Checkout {username} for searching files'
-INVITE_MSG = environ.get('INVITE_MSG', 'Please join @.... to use this bot')
+SHARE_BUTTON_TEXT = 'Checkout {filmsreqbot} for searching files'
+INVITE_MSG = environ.get('INVITE_MSG', 'Please go to @filmsbotreq to use this bot. Exclusive for group members.')
